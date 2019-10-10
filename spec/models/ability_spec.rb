a2a1250a51d690116d56b2c12ee680c9c83c9ff4
John Hu <huziyong@gmail.com>
2019-10-10 10:59:50 +0800

    Defines abilities to models via cancancan

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'cancan/matchers'
+
+RSpec.describe Ability, type: :model do
+  describe "abilities" do
+    subject(:ability) { described_class.new(user) }
+
+    let(:user) { create :user }
+
+    context "when reads a book" do
+      let(:book) { create :book }
+
+      it { is_expected.to be_able_to(:read, book) }
+    end
+
+    context "when creates a book" do
+      let(:book) { create :book, creator: user }
+
+      it { is_expected.to be_able_to(:update, book) }
+    end
+
+    context "when print_book is shared" do
+      let(:print_book) { create :print_book, property: :shared }
+
+      it { is_expected.to be_able_to(:read, print_book) }
+    end
+
+    context "when print_book is personal" do
+      let(:print_book) { create :print_book, property: :personal }
+
+      it { is_expected.not_to be_able_to(:read, print_book) }
+    end
+
+    context "when print_book is personal by self" do
+      let(:print_book) { create :print_book, property: :personal, owner: user }
+
+      it { is_expected.to be_able_to(:read, print_book) }
+    end
+
+    context "when owns a print_book" do
+      let(:print_book) { create :print_book, owner: user }
+
+      it { is_expected.to be_able_to(:manage, print_book) }
+    end
+
+    context "when holds a print_book" do
+      let(:print_book) { create :print_book, holder: user }
+
+      it { is_expected.to be_able_to(:update, print_book) }
+    end
+
+    context "when holds a print_book" do
+      let(:print_book) { create :print_book, holder: user }
+
+      it { is_expected.not_to be_able_to(:destroy, print_book) }
+    end
+
+    context "when print_book is shared" do
+      let(:print_book) { create :print_book, property: :shared }
+
+      it { is_expected.to be_able_to(:read, print_book) }
+    end
+
+    context "when reads a deal" do
+      let(:deal) { create :deal }
+
+      it { is_expected.to be_able_to(:read, deal) }
+    end
+
+    context "when sponsored a deal" do
+      let(:deal) { create :deal, sponsor: user }
+
+      it { is_expected.to be_able_to(:update, deal) }
+    end
+
+    context "when received a deal" do
+      let(:deal) { create :deal, receiver: user }
+
+      it { is_expected.to be_able_to(:update, deal) }
+    end
+
+    context "when applied a deal" do
+      let(:deal) { create :deal, applicant: user }
+
+      it { is_expected.to be_able_to(:update, deal) }
+    end
+  end
+end
