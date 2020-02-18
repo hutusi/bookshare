1abe0910fdadae2c97494ebfb1ef57a74ef21164
John Hu <huziyong@gmail.com>
2020-02-18 21:08:46 +0800

    Bundle update
    update rails from 6.0 to 6.0.2.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.12"
 
 set :application, "bookshare"
 set :repo_url, "git@github.com:hutusi/bookshare.git"
