ab2fab9954bb6f5ebf7c7e0bbd643fefde403f07
John Hu <huziyong@gmail.com>
2020-03-26 22:32:41 +0800

    Upgrade webpack to fix complie error

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require('tailwindcss'),
+    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
