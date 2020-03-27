dc212a09d11a5ec5101b84bf816d02ff650b0a88
John Hu <huziyong@gmail.com>
2020-03-27 21:42:45 +0800

    Refactor home#index view

--- config/initializers/1_app.rb
+++ config/initializers/1_app.rb
@@ -2,5 +2,5 @@ module Bookshare
   VERSION = File.file?(Rails.root.join('VERSION')) ? 
     File.read(Rails.root.join('VERSION')).strip.freeze : 'No VERSION'
   REVISION = File.file?(Rails.root.join('REVISION')) ?
-  File.read(Rails.root.join('REVISION')).strip.freeze : 'No REVISION'
+  File.read(Rails.root.join('REVISION')).strip.first(7).freeze : 'No REVISION'
 end
